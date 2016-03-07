f057d9d2da0c5137e7bb870b636587b256642442
alexandregama <alexandre.gama.lima@gmail.com>
2016-03-07 19:16:54 -0300

    Transforming a body message using a TransformerBean class

--- src/main/java/com/mastering/camel/transform/TransformAndAppendStringBody.java
+++ src/main/java/com/mastering/camel/transform/TransformAndAppendStringBody.java
@@ -1,11 +1,14 @@
-package com.mastering.camel;
+package com.mastering.camel.transform;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.camel.CamelContext;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
-public class TransformFileBody {
+public class TransformAndAppendStringBody {
 
 	public static void main(String[] args) throws Exception {
 		CamelContext context = new DefaultCamelContext();
@@ -14,20 +17,21 @@ public class TransformFileBody {
 			
 			@Override
 			public void configure() {
-				from("file:entry?delay=5s").
+				from("file:entry?delay=3s").
 					log(LoggingLevel.INFO, "Processing the following message: ${id}").
-					transform(bodyAs(String.class).regexReplaceAll("nomeAutor", "nameAuthor")).
+					transform(bodyAs(String.class).regexReplaceAll("autor", "nameAuthor")).
+					setBody(body().append("From day: " + new SimpleDateFormat("dd/MM/yyyy").format(new Date()))).
 				to("file:exit");	
 			}
 		});
-		
+			
 		context.start();
 		
 		Thread.sleep(3_000);
 		
 		context.stop();
 		
-		System.out.println("Camel has been stoped");
+		System.out.println("Camel has been stoped!");
 	}
 	
 }
